31f8714230a741710ca8111490915a70e759f4ed
yuhsssss <167497897+yuhsssss@users.noreply.github.com>
2025-05-08 20:54:16 +0800

    Create env.d.ts

---
+++ src/_types_/env.d.ts
@@ -0,0 +1,6 @@
+declare module '@env' {
+  export const AZURE_OPENAI_ENDPOINT: string;
+  export const API_KEY: string;
+  export const DEPLOYMENT_NAME: string;
+  export const API_VERSION: string;
+}
